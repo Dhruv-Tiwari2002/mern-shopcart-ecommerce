896be9001d043a39f369ce24f8bf8282d3300d64
Dhruv Tiwari <ddhruvtiwari@gmail.com>
2026-09-21 01:42:18 +0530

    Update frontend API URL to point to Render

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://mern-shopcart-ecommerce.onrender.com',
+  withCredentials: true,
 });
 
 // Automatically attach the JWT token to every request
